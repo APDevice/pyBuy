d3a045615e4f115365f244eb9218e14fd4737812
APDevice <59993100+APDevice@users.noreply.github.com>
2021-12-13 03:11:16 -0500

    added tests for passing tokens

--- test_api.py
+++ test_api.py
@@ -1,5 +1,5 @@
 import unittest
-from api import Scope, Token
+from api import Scope, Search, Token
 
 AUTH_ID = "DylanLut-pyBuy-SBX-47ca6edf0-a73f32d9"
 CLIENT_ID = "SBX-7ca6edf0bf2a-6443-4f6d-89e1-32be"
@@ -29,3 +29,30 @@ class Test_API(unittest.TestCase):
         t = Token(AUTH_ID, CLIENT_ID, [Scope.PUBLIC], sandbox=False)
         
         self.assertEquals( t._get_sandbox(), "")
+        
+    def test_token_passed_to_result(self):
+        t = Token(AUTH_ID, CLIENT_ID, [Scope.PUBLIC], sandbox=True)
+        
+        expected = t.get_token()
+        
+        s = Search(t)
+        results = s.new_search().keywords( "something" ).execute()
+        
+        got = results.get_token().get_token()
+        
+        self.assertEquals(expected, got)
+        
+    def test_token_passed_to_next_result(self):
+        t = Token(AUTH_ID, CLIENT_ID, [Scope.PUBLIC], sandbox=True)
+
+        s = Search(t)
+        results = s.new_search().keywords( "playstation" ).execute()
+        
+        expected = results.get_token().get_token()
+        
+        results = results.next()
+        
+        got = results.get_token().get_token()
+        
+        self.assertEquals(expected, got)
+        
