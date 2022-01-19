a59c2637bc9fe445c0a4ec1a14620dd410295300
APDevice <59993100+APDevice@users.noreply.github.com>
2022-01-18 23:07:20 -0800

    fixed version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='pybuy',
-    version='0.0.3',    
+    version='0.3.0',    
     description='A library for accessing Ebay REST API',
     url='https://github.com/APDevice/pyBuy',
     author='Dylan Luttrell',
